37ff7d611127ea0065adc45f45ac8e878d539ef8
khushal2772 <khushalsingh9b14@gmail.com>
2025-06-24 15:16:05 +0530

    jl

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-function a=
+let [ hour, minute, second, milisecond]= [0,0,0,0];
+function stopwatch() {
+    milisecond++;
+    if (milisecond===100) {
+        milisecond=0;
+        second++;
+            if (second===60) {
+            second=0;
+            minute++;
+                if (minute===60) {
+                minute=0;
+                hour++;
+                    if (hour===24) {
+                    hour=0;
+                };
+            };
+        };
+    };
+}
+
+function start() {
+    timer = setInterval(() => {
+    stopwatch()
+    console.log(`${hour}:${minute}:${second}:${milisecond}`);
+    }, 10);
+}
+function stop() {
+    clearInterval(timer)
+}
+function reset() {
+    hour=minute=second=milisecond=0;
+}
+function lap() {
+    const laptime = (`${hour}:${minute}:${second}:${milisecond}`)
+    console.log(laptime);
+    
+}
